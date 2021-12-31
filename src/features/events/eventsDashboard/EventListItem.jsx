922fbe3dba91f366a258a1f3435fd6a148831dc9
casteser <61086379+SCASTELLANO6044@users.noreply.github.com>
2021-12-31 15:10:25 +0100

    Adding components to the front page

--- src/features/events/eventsDashboard/EventListItem.jsx
+++ src/features/events/eventsDashboard/EventListItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Item, Segment } from "semantic-ui-react";
+import { Icon, Item, List, Segment } from "semantic-ui-react";
 
 export default function EventListItem(){
     return(
@@ -8,8 +8,25 @@ export default function EventListItem(){
                 <Item.Group>
                     <Item>
                         <Item.Image size='tiny' circular src='/assets/user.png'/>
+                        <Item.Content>
+                            <Item.Header content='Event title'/>
+                            <Item.Description>
+                                Hosted by someone.
+                            </Item.Description>
+                        </Item.Content>
                     </Item>
                 </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock'/> Date
+                    <Icon name='marker'/> Venue
+                </span>
+            </Segment>
+            <Segment secondary>
+                <List>
+                    
+                </List>
             </Segment>
         </Segment.Group>
     )
