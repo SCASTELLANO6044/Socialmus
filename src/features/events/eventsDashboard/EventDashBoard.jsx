5e855c4fde0acf277cd9cde9713c85a3c9fbe257
casteser <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-01-05 13:24:46 +0100

    Passing properties down to child components

--- src/features/events/eventsDashboard/EventDashBoard.jsx
+++ src/features/events/eventsDashboard/EventDashBoard.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import EventForm from '../eventForm/EventForm';
 import EventList from './EventList';
+import { sampleData } from '../../../app/api/sampleData';
 
 export default function EventDashBoard (){
     return(
         <Grid>
             <Grid.Column width={8}>
-                <EventList/>
+                <EventList events={sampleData}/>
             </Grid.Column>
             <Grid.Column width={8}>
                 <EventForm/>
